076ae4ba404750e207ce52dc92bfb35989e8b7c4
Henry Cookson <henry.cookson@rocketmail.com>
2022-10-10 11:40:17 +0100

    add task 2 up to the end of auth flow

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,21 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// Run this code when a form is submitted to '/auth/v9/gov-account-3'
+router.post('/auth/v9/gov-account-3', function (req, res) {
+
+    // Make a variable and give it the value from 'do-you-think-you-have-a-trn'
+    var doYouThinkYouHaveATRN = req.session.data['do-you-think-you-have-a-trn']
+  
+    // Check whether the variable matches a condition
+    if (doYouThinkYouHaveATRN == "yes"){
+      // Send user to auth flow
+      res.redirect('/auth/v9/gov-account-3')
+    } else {
+      // Do nothing
+      res.redirect('/find-a-lost-teacher-reference-number/check-if-you-have-a-trn')
+    }
+  
+  })
+
 module.exports = router
